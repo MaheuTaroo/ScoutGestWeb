b3c6865ebb5651c322ebdf68a782767453e2a15e
MaheuTaroo <pantupa@gmail.com>
2020-06-11 12:21:40 +0100

    Continuação da programação

---
+++ ScoutGestWeb/Controllers/InfoLogoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScoutGestWeb.Controllers
+{
+    public class InfoLogoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (UserData.UserData.userData.Count == 0) return RedirectToAction("Index", "Login");
+            return View();
+        }
+    }
+}
